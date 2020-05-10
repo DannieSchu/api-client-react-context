cca4ec6b3111182f471e051ac1ca143c2a1750df
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-10 00:12:04 -0700

    modify JsonDisplay component to use global state

--- src/components/JsonDisplay/JsonDisplay.jsx
+++ src/components/JsonDisplay/JsonDisplay.jsx
@@ -1,34 +1,32 @@
-/* eslint-disable indent */
-/* eslint-disable react/prop-types */
 import React from 'react';
 import ReactJson from 'react-json-view';
-import PropTypes from 'prop-types';
+import { useResults, useLoading } from '../../hooks/APIClientProvider';
 import { themeColors } from './themeColors.jsx';
 import styles from './JsonDisplay.css';
 
-const JsonDisplay = ({ results, loading }) => (
-  <section className={styles.JsonDisplay}>
-    <h2>Result</h2>
-    <article>
-      {!results && loading
-        ? 'Loading' 
-        : results
-        ? <ReactJson
-          src={results}
-          theme={themeColors}
-          enableClipboard={false}
-          iconStyle="circle"
-          displayDataTypes={false}
-          groupArraysAfterLength={50}
-        />
-        : 'Press send to get a response'}
-    </article>
-  </section >
-);
+const JsonDisplay = () => {
+  const { results } = useResults();
+  const { loading } = useLoading();
 
-JsonDisplay.propTypes = {
-  results: PropTypes.any,
-  loading: PropTypes.bool
+  return (
+    <section className={styles.JsonDisplay}>
+      <h2>Result</h2>
+      <article>
+        {!results && loading
+          ? 'Loading'
+          : results
+            ? <ReactJson
+              src={results}
+              theme={themeColors}
+              enableClipboard={false}
+              iconStyle="circle"
+              displayDataTypes={false}
+              groupArraysAfterLength={50}
+            />
+            : 'Press \'send\' to get a response'}
+      </article>
+    </section >
+  );
 };
 
 export default JsonDisplay;
