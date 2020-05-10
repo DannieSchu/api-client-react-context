ad293f48d9c52de95307f78437a3745e91db0a63
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-10 00:15:57 -0700

    modify RequestHistory component to use global state

--- src/components/RequestHistory/RequestHistory.jsx
+++ src/components/RequestHistory/RequestHistory.jsx
@@ -1,33 +1,26 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import RequestItem from './RequestItem.jsx';
+import { useRequests, useHandleDelete } from '../../hooks/APIClientProvider.jsx';
 import { weakKey } from '../../utilities/weakKey.jsx';
 import styles from './RequestHistory.css';
 import buttonStyle from '../../css/button.css';
 
-const RequestHistory = ({ requests, onClick, onDelete }) => {
+const RequestHistory = () => {
+  const requests = useRequests();
+  const handleDelete = useHandleDelete();
+
   const requestElements = requests.map(request => (
     <li key={weakKey(request)}>
-      <RequestItem {...request} onClick={() => onClick(request)} />
+      <RequestItem {...request} />
     </li>
   ));
   
   return (
     <ul className={styles.RequestHistory}>
       {requestElements}
-      <button className={buttonStyle.tealButton} onClick={onDelete}>Clear History</button>
+      <button className={buttonStyle.tealButton} onClick={handleDelete}>Clear History</button>
     </ul>
   );
-};
-
-RequestHistory.propTypes = {
-  onClick: PropTypes.func.isRequired,
-  onDelete: PropTypes.func.isRequired,
-  requests: PropTypes.arrayOf(PropTypes.shape({
-    method: PropTypes.string.isRequired,
-    url: PropTypes.string.isRequired,
-    body: PropTypes.string,
-  }))
 };
 
 export default RequestHistory;
--- src/components/RequestHistory/RequestHistory.test.jsx
+++ src/components/RequestHistory/RequestHistory.test.jsx
@@ -1,25 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import RequestHistory from './RequestHistory.jsx';
+import { APIClientProvider } from '../../hooks/APIClientProvider.jsx';
 
 describe('RequestHistory', () => {
   it('matches a snapshot', () => {
-    const requests = [{
-      method: 'post', 
-      url: 'https://jsonplaceholder.typicode.com/posts'
-    }, {
-      method: 'get', 
-      url: 'https://jsonplaceholder.typicode.com/posts'
-    }, {
-      method: 'put', 
-      url: 'https://jsonplaceholder.typicode.com/posts'
-    }, {
-      method: 'put', 
-      url: 'https://jsonplaceholder.typicode.com/posts'
-    }];
 
     const wrapper = shallow(
-      <RequestHistory requests={requests} onClick={() => {}}onDelete={() => {}} />
+      <APIClientProvider>
+        <RequestHistory />
+      </APIClientProvider>
     );
     expect(wrapper).toMatchSnapshot();
   });
