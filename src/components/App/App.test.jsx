63f5798f3e5d7031c0c176d65d3e01c33ab9592c
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-10 00:05:06 -0700

    create context provider

--- src/components/App/App.test.jsx
+++ src/components/App/App.test.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import { APIClientProvider } from '../../hooks/APIClientProvider';
 
 describe('App component', () => {
   it('renders App', () => {
-    const wrapper = shallow(<App />);
+    const wrapper = shallow(
+      <APIClientProvider>
+        <App />
+      </APIClientProvider>
+    );
     expect(wrapper).toMatchSnapshot();
   });
 });
